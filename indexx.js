8d5cc702843ccb13615dc66c524f5f31d076d66d
krish-yadav1921 <krishraj54082@gmail.com>
2025-08-09 12:17:54 +0530

    Update indexx.js

--- indexx.js
+++ indexx.js
@@ -1,8 +1,22 @@
 function f(k) {
-	if(Math.abs(k) > .5)
-		scrollTo(0, .5*(k - Math.sign(k) + 1)*(document.documentElement.offsetHeight - window.innerHeight))
+  if (Math.abs(k) > 0.5) {
+    scrollTo({
+      top: 0.5 * (k - Math.sign(k) + 1) * (document.documentElement.scrollHeight - window.innerHeight),
+      behavior: 'smooth'
+    });
+  }
 }
 
 f(-1);
 
-addEventListener('scroll', e => f(+getComputedStyle(document.body).getPropertyValue('--k')))
+let ticking = false;
+addEventListener('scroll', () => {
+  if (!ticking) {
+    requestAnimationFrame(() => {
+      const k = +getComputedStyle(document.body).getPropertyValue('--k');
+      f(k);
+      ticking = false;
+    });
+    ticking = true;
+  }
+});
